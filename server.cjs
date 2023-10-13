5a97b9c0428ddbcc71470396b8869daf0d809825
Exdel-li <147731335+Exdel-li@users.noreply.github.com>
2023-10-13 18:30:45 +0000

    First commit

--- server.cjs
+++ server.cjs
@@ -1,49 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose');
-
-const app = express();
-const port = process.env.PORT || 5000;
-
-// MongoDB Connection
-mongoose.connect('mongodb+srv://Exdel:<weneedmoeny>@cluster0.klys9qy.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-const linkSchema = new mongoose.Schema({
-    originalURL: String,
-    generatedURL: String,
-    clickCount: Number,
-    expirationDate: Date,
-});
-
-const Link = mongoose.model('Link', linkSchema);
-
-// Express Middleware
-app.use(express.json());
-
-// Express Routes
-app.post('/generate-link', async (req, res) => {
-    const originalURL = req.body.originalURL;
-    
-    // Generate a unique link.
-    const generatedURL = await generateUniqueLink();
-    const clickCount = 0;
-    const expirationDate = new Date();
-    expirationDate.setDate(expirationDate.getDate() + 6); // Six days from now.
-
-    // Save the link in the database.
-    const newLink = new Link({ originalURL, generatedURL, clickCount, expirationDate });
-    await newLink.save();
-
-    res.json({ generatedURL });
-});
-
 app.get('/:uniqueString', async (req, res) => {
     const uniqueString = req.params.uniqueString;
 
     // Find the link in the database.
-    const link = await Link.findOne({ generatedURL: 'https://adlogx.wixsite.com/' + uniqueString });
+    const link = await Link.findOne({ generatedURL: 'https://redir-exdel-li.onrender.com/' + uniqueString });
 
     if (!link) {
         return res.status(404).json({ error: 'Link not found' });
@@ -58,34 +17,12 @@ app.get('/:uniqueString', async (req, res) => {
     link.clickCount++;
     await link.save();
 
-    // Redirect to the original URL.
-    res.redirect(link.originalURL);
-});
-
-async function generateUniqueLink() {
-    let generatedURL;
-    let isUnique = false;
-    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-
-    while (!isUnique) {
-        generatedURL = 'https://adlogx.wixsite.com/' + generateRandomCharacters(8, characters);
-        const existingLink = await Link.findOne({ generatedURL });
-        if (!existingLink) {
-            isUnique = true;
-        }
+    // Redirect based on click count.
+    if (link.clickCount <= 7) {
+        res.redirect('https://adlogx.wixsite.com/fortin');
+    } else if (link.clickCount <= 12) {
+        res.redirect('https://adlogx.wixsite.com/404error');
+    } else {
+        res.redirect('https://adlogx.wixsite.com/crogan');
     }
-    return generatedURL;
-}
-
-function generateRandomCharacters(length, characters) {
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
-}
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
 });
